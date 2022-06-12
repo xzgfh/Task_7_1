e4de52015c187e430705d76b877b874404ea0ba5
xzgfh <User@WIN-Q06VGDO4GD9>
2022-06-12 15:34:45 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Task_7_1
+{
+    class Test
+    {
+        private string Nname;
+        public String Name {
+            get { return Nname;}
+            set { Nname = value; }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> list = new List<object>() { 1,new Test() { Name = "456" } , 3, 4, new Test() { Name = "456" } };
+            object[] array = new object[] { 1, list, new Test() { Name = "789" } };
+            //object obj = array;
+
+            foreach (var VAR in Reflection.ConstructEnumerator(array))
+            {
+                Console.WriteLine(VAR);
+            }
+            Console.WriteLine();
+            foreach (var VAR in Reflection.ConstructEnumerator1(array))
+            {
+                Console.WriteLine(VAR);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Reflection.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Runtime.Remoting.Messaging;
+
+namespace Task_7_1
+{
+    class Reflection
+    {
+        static public IEnumerable ConstructEnumerator(object obj)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue(obj);
+            while (queue.Count > 0)
+            {
+                object n_obj = queue.Dequeue();
+                Type type = n_obj.GetType();
+                
+                PropertyInfo pInfo = type.GetProperty("Name", typeof(string));
+                
+                if (pInfo != null && pInfo.GetGetMethod() != null && pInfo.GetSetMethod() != null)
+                {
+                    
+                    yield return n_obj;
+                }
+                else if (IsList(n_obj)) 
+                {
+                    
+
+                    foreach (var VAR in (IEnumerable)n_obj)
+                    {
+                       
+                        queue.Enqueue(VAR);
+                    }
+                }
+            }
+        }
+
+        static public IEnumerable ConstructEnumerator1(object obj)
+        {
+          Type type = obj.GetType();
+
+          PropertyInfo pInfo = type.GetProperty("Name", typeof(string));
+          if (pInfo != null && pInfo.GetGetMethod() != null && pInfo.GetSetMethod() != null)
+          {
+            yield return obj;
+          }
+          else if (IsList(obj))
+          {
+              foreach (var VAR in (IEnumerable)obj)
+              {
+                  foreach (var n_obj in ConstructEnumerator1(VAR))
+                  {
+                      yield return n_obj;
+                  }
+              }
+          }
+        }
+
+        public static bool IsList(object obj)
+        {
+            return (obj is IList);
+        }
+
+        public static bool IsGenericList(object obj)
+        {
+            return (obj is IList && obj.GetType().IsGenericType);
+        }
+    }
+}
